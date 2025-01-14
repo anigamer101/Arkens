0cae71f6dda772991f4521903a28c00759fd91fe
anigamer101 <102794353+anigamer101@users.noreply.github.com>
2025-01-14 14:29:34 +0530

    Part-1

--- Arkens.py
+++ Arkens.py
@@ -1,15 +1,20 @@
 #Imports
 from tkinter import *
+import os
 
 #Gui
 win=Tk()
 win.geometry("1028x1028")
 def get_input():
-   value=Linkbox.get("1.0","end-1c")
-   print(value)
+   global link
+   link = Linkbox.get("1.0","end-1c")
+   print(link)
 Linkbox=Text(win, height=5, width=25)
 Linkbox.pack()
 Linkbut= Button(win, height=5, width=25, text="Link", command=lambda: get_input())
 Linkbut.pack()
+
+#Error management
+path = os.access(link,os.F_OK)
 
 win.mainloop()
