55efabcff38292f0344b0fb2feafe71d17f57e4d
anigamer101 <102794353+anigamer101@users.noreply.github.com>
2025-01-15 18:20:39 +0530

    4

--- Arkens.py
+++ Arkens.py
@@ -11,14 +11,14 @@ from tkinter import messagebox
 win=Tk()
 win.geometry("1028x1028")
 
-#Text box 1(REPO)
+#Text box 1(Clone)
 def get_input1():
       global link
       link = Linkbox.get("1.0","end-1c")
 Linkbox=Text(win, height=5, width=25)
 Linkbox.pack()
 
-Linkbut= Button(win, height=1, width=25, text="Cloned repo branch folder", command=lambda: get_input1())
+Linkbut= Button(win, height=1, width=25, text="Cloned repo folder", command=lambda: get_input1())
 Linkbut.pack()
 
 #Text box 2(Token)
@@ -32,6 +32,16 @@ Blinkbox.pack()
 Blinkbut= Button(win, height=1, width=25, text="Token Name", command=lambda: get_input2())
 Blinkbut.pack()
 
+#Text box 3(Repo)
+def get_input3():
+      global repo
+      repo = rebox.get("1.0","end-1c")
+rebox=Text(win, height=5, width=25)
+rebox.pack()
+
+Rebut= Button(win, height=1, width=25, text="Repo link", command=lambda: get_input3())
+Rebut.pack()
+
 
 #Token Creation
 def seti():
@@ -40,6 +50,11 @@ def seti():
       if not os.file.exists(link):
             f = open(Slink, "x")
             print(Slink)
-      else : messagebox.showerror("Arkens","Error : Token exists")
+            repo.index.add([Slink]) 
+            print('Files Added Successfully') 
+            repo.index.commit(Slink) 
+            print('Commited successfully')
+      else : 
+            messagebox.showerror("Arkens","Error : Token exists")
       
 win.mainloop()
