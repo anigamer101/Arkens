cb2bb6262de9181138af1469cb92546c2e6961ab
anigamer101 <102794353+anigamer101@users.noreply.github.com>
2025-01-17 11:33:37 +0530

    5

--- Arkens.py
+++ Arkens.py
@@ -3,51 +3,63 @@ import git
 from tkinter import *
 import os
 from tkinter import messagebox
+import tkinter as tk
+import os.path
+from PIL import Image, ImageTk
+from pathlib import Path
 
-
-
-      
 #GUI Init
 win=Tk()
 win.geometry("1028x1028")
+win.title("Arkens")
 
-#Text box 1(Clone)
-def get_input1():
-      global link
+
+#Label variables
+lin = tk.StringVar()
+rep = tk.StringVar()
+blin = tk.StringVar()
+ex = tk.StringVar()
+rep.set("Git repository digital link")
+blin.set("Name your token")
+ex.set("Expire after (Textbox input) seconds from now")
+lin.set("Name your token")
+
+#Submit button execution code
+def get_input():
+      global repo, exp, Blink, link
       link = Linkbox.get("1.0","end-1c")
+      repo = rebox.get("1.0","end-1c")
+      exp = exbox.get("1.0","end-1c")
+      seti()
+
+#Text box 1 (Token)
+linlabel = tk.Label(win, textvariable=lin, height=3, width=30, bd=3,)
+linlabel.pack(pady=20)
 Linkbox=Text(win, height=5, width=25)
 Linkbox.pack()
 
-Linkbut= Button(win, height=1, width=25, text="Cloned repo folder", command=lambda: get_input1())
-Linkbut.pack()
 
-#Text box 2(Token)
-def get_input2():
-      global Blink
-      Blink = Blinkbox.get("1.0","end-1c")
-      seti()
-Blinkbox=Text(win, height=5, width=25)
-Blinkbox.pack()
-
-Blinkbut= Button(win, height=1, width=25, text="Token Name", command=lambda: get_input2())
-Blinkbut.pack()
-
-#Text box 3(Repo)
-def get_input3():
-      global repo
-      repo = rebox.get("1.0","end-1c")
+#Text box 2 (Repo)
+relabel = tk.Label(win, textvariable=rep, height=3, width=30, bd=3,)
+relabel.pack(pady=20)
 rebox=Text(win, height=5, width=25)
 rebox.pack()
 
-Rebut= Button(win, height=1, width=25, text="Repo link", command=lambda: get_input3())
-Rebut.pack()
+#Text box 3 (Valid until)
+exlabel = tk.Label(win, textvariable=ex, height=3, width=40, bd=3,)
+exlabel.pack(pady=20)
+exbox =Text(win, height = 5, width = 25)
+exbox.pack()
 
+#Submit Button
+subut= Button(win, height=1, width=25, text="Repo link", command=lambda: get_input())
+subut.pack()
 
 #Token Creation
 def seti():
       File = [link,".txt"]
       Slink = " ".join(File)
-      if not os.file.exists(link):
+      if not  os.path.exists(link):
             f = open(Slink, "x")
             print(Slink)
             repo.index.add([Slink]) 
