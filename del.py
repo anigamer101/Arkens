8f4acdb850cc0b16a495c9efde685cb1c51c153d
anigamer101 <102794353+anigamer101@users.noreply.github.com>
2025-01-17 18:35:46 +0530

    19

--- del.py
+++ del.py
@@ -4,5 +4,6 @@ import os
 from Arkens import Slink, expi
 time.sleep(expi)
 os.remove(Slink)
+subprocess.call(["git", "checkout", "main"])
 subprocess.call(["git", "commit", Slink])
-subprocess.call(["git", "push",])
+subprocess.call(["git", "push", "-u", "tokens"])
