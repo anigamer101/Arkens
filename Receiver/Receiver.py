96a34c838e457174e058a47c2da8d6ab892ef97f
anigamer101 <102794353+anigamer101@users.noreply.github.com>
2025-02-01 20:19:23 +0530

    Part 3

--- Receiver/Receiver.py
+++ Receiver/Receiver.py
@@ -6,7 +6,7 @@ import tkinter as tk
 #GUI
 win=Tk()
 win.geometry("1028x1028")
-win.title("Eye Shield") 
+win.title("Eye Shield")
 
 #Label Variables
 Tla = tk.StringVar()
@@ -23,18 +23,21 @@ subut= Button(win, height=1, width=25, text="check", command=lambda: yeti())
 subut.pack()
 
 #Github
-g = Github("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")
+global contents
+g = Github("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")
 repo = g.get_repo("anigamer101/Arkens")
 contents = repo.get_contents("")
 
+
 #Check if a file is present
 def yeti() :
+    global sr
     var =  Tbox.get("1.0", "end-1c")
     for content in contents :
         if content.name in var:
-            print("yes!")
-            win.destroy()
-        else: 
-            print("no!")
-            win.destroy()
+            sr = 1
+    if sr == 1 :
+        print("yes!")
+    else: 
+        print("no!")
 win.mainloop()
